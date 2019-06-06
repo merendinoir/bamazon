4a263183380a4836ab9f7d9e3abe6b737b451f27
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-06-06 16:28:14 -0400

    changed columns

--- bamazon.sql
+++ bamazon.sql
@@ -6,7 +6,7 @@ CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
   item VARCHAR(100) NULL,
   department VARCHAR(100) NULL,
-  price DECIMAL(10,2) NULL,
+  price DEC(10,2) NULL,
   quantity INT NULL,
   PRIMARY KEY (id)
 )
