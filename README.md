9c57e4591ba2e972828de18f4a07a5b2e051c6c1
Isabella Merendino <isabellamerendino@Isabellas-MacBook-Pro.local>
2019-06-06 16:17:45 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bamazon
 week 12 homework
+
+# Node.js & MySQL
+
+Create an Amazon-like storefront using MySQL. The app will take in orders from customers and deplete stock from the store's inventory. 
+
+As a bonus task, you can program your app to track product sales across your store's departments and then provide a summary of the highest-grossing departments in the store.
---
+++ bamazon.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "8236980Hey",
+  database: "bamazonDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+
+//function goes here like display table
+
+});
